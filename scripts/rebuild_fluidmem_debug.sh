554e85f0e8140c76b7963540a0b0f5189573d777
Blake Caldwell <blake.caldwell@colorado.edu>
2018-12-23 22:20:29 +0000

    Fix script for rebuilding fluidmem with debugging
    Change-Id: I09c2b7c87e73a51c018851cf9552ae0ffa771ea6

--- scripts/rebuild_fluidmem_debug.sh
+++ scripts/rebuild_fluidmem_debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
-configure_cmd=$(grep -e "^  \$ \.\/configure " /scaleos/config.log|sed 's/  \$ //')
-cd /scaleos
+configure_cmd=$(grep -e "^  \$ \.\/configure " /fluidmem/config.log|sed 's/  \$ //')
+cd /fluidmem
 make distclean
 eval "$configure_cmd --enable-debug --enable-trace --enable-lock-debug"
 make -j5
